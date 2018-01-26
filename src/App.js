6fb2e935a03a3c2650725c12f3608987af53c96d
gaspaonrocks <gr.sacha.glgm@gmail.com>
2018-01-26 15:12:18 +0100

    toolbar finished

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 import Toolbar from "./toolbar/Toolbar";
+import Menu from "./menu/Menu";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Toolbar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*
+        * Maybe create a full width Main view
+        * then inside have a side menu
+        * and have a content view in the middle...
+        */}
+        <Menu />
       </div>
     );
   }
--- src/toolbar/Toolbar.js
+++ src/toolbar/Toolbar.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
+import "./Toolbar.css";
+import "font-awesome/css/font-awesome.min.css";
 
 class Toolbar extends Component {
   render() {
     return (
       <div>
-        <button>This is logo</button>
-        <div class="hidden">
-          <ul>
-            <li>This is menu1</li>
-            <li>This is menu2</li>
-            <li>This is menu3</li>
-            <li>This is menu4</li>
-          </ul>
+        <div className="navbar is-navbar">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="https://github.com/gaspaonrocks/pomedapi">
+              <img src="https://www.avisdemamans.com/images/article/2013-header-logo-pomme-d-api_link-header.png" alt="Pomedapi: best daycare for children... and their parents" />
+            </a>
+            <div className="button navbar-burger" >
+              <span />
+              <span />
+              <span />
+            </div>
+          </div>
+
+          <div className="navbar-menu">
+            <div className="navbar-start">
+              <a className="navbar-item"><i className="fa fa-home fa-2x icon-white" aria-hidden="true"></i></a>
+            </div>
+
+            <div className="navbar-end">
+            <a className="navbar-item"><i className="fa fa-envelope fa-2x icon-white" aria-hidden="true"></i></a>
+            </div>
+          </div>
         </div>
       </div>
     );
